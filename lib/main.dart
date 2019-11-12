f867a7c1c7ddd27c50e02c1fbbaa56e9dbff3f99
zwtin <masa100928@gmail.com>
2019-11-13 08:06:28 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'dart:convert';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Issues',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Flutter Issues'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class Issue {
+  Issue({
+    this.title,
+    this.avatarUrl,
+  });
+
+  final String title;
+  final String avatarUrl;
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Issue> _issues = <Issue>[];
+
+  @override
+  void initState() {
+    super.initState();
+    _load();
+  }
+
+  Future<void> _load() async {
+    final res = await http.get('https://api.github.com/repositories/31792824/issues');
+    final data = json.decode(res.body);
+    setState(() {
+      final issues = data as List;
+      issues.forEach((dynamic element) {
+        final issue = element as Map;
+        _issues.add(Issue(
+          title: issue['title'] as String,
+          avatarUrl: issue['user']['avatar_url'] as String,
+        ));
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: ListView.builder(
+        itemBuilder: (BuildContext context, int index) {
+          if (index >= _issues.length) {
+            return null;
+          }
+
+          final issue = _issues[index];
+          return ListTile(
+            leading: ClipOval(
+              child: Image.network(issue.avatarUrl),
+            ),
+            title: Text(issue.title),
+            onTap: () => onTapped(issue),
+          );
+        },
+      ),
+    );
+  }
+
+  void onTapped(Issue issue) {
+    debugPrint(issue.avatarUrl);
+  }
+}
